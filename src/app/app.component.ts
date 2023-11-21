26084256400703acaa8f977787debacc57f7a0c7
saloum45 <saloumfall45@gmail.com>
2023-11-21 09:22:17 +0000

    login okay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GestionNote_G2';
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  // Attributs
+  email="";
+  pass="";
 
+  // Methodes
   constructor(private router:Router){
 
   }
   connexion(){
-    this.router.navigate(['admin']);
+    if (this.email=="" || this.pass=="") {
+      Swal.fire({
+        title: "Désolé",
+        text: "Veuillez renseigner tous les champs",
+        icon: "error"
+      });
+    }else if(this.email!="admin@gmail.com" || this.pass!="passer"){
+      Swal.fire({
+        title: "Désolé",
+        text: "Vérifier la saisie",
+        icon: "error"
+      });
+    }else{
+      this.router.navigate(['admin']);
+      Swal.fire({
+        title: "Bienvenue",
+        text: "Connexion faite avec succes",
+        icon: "success"
+      });
+    }
   }
 }
